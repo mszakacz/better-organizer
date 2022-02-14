159fc5c3ae72445159d737fec7f868be7d2a58f3
Maciej Szakacz <maciejszakacz@maciejs-macbook-pro-1.home>
2022-02-14 12:24:11 +0100

    adding copy-with to Contact class

--- packages/contact_list_repository/lib/src/models/contact.dart
+++ packages/contact_list_repository/lib/src/models/contact.dart
@@ -14,4 +14,22 @@ class Contact {
     this.address = '',
     this.description = '',
   });
+
+  Contact copyWith({
+    String? name,
+    String? lastname,
+    String? mobile,
+    String? mail,
+    String? address,
+    String? description,
+  }) {
+    return Contact(
+      name: name ?? this.name,
+      lastname: lastname ?? this.lastname,
+      mobile: mobile ?? this.mobile,
+      mail: mail ?? this.mail,
+      address: address ?? this.address,
+      description: description ?? this.description,
+    );
+  }
 }
