33c25a13a0a2703e7dc7ff4af8d20ca98099705a
Maciej Szakacz <maciejszakacz@maciejs-macbook-pro-1.home>
2022-02-16 02:22:34 +0100

    contact List repository conected with FireBase and sendung Stream of ContactList

---
+++ packages/contact_list_repository/lib/src/repositories/contact_list_repository.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import '../models/models.dart';
+
+abstract class ContactListRepository {
+  Stream<List<Contact>> constactList();
+  void dispose();
+  void refresh();
+}
+
+class ContactListRepositoryMemory extends ContactListRepository {
+  StreamController<List<Contact>> _constactList = StreamController();
+
+  @override
+  void refresh() {
+    _constactList.add(
+      []..add(
+          const Contact(
+            name: 'Marcus',
+            lastname: 'Smart',
+            mobile: '25436785675',
+            mail: 'smart13@gmail.com',
+            address: 'Boston',
+            description: '',
+          ),
+        ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _constactList.close();
+  }
+
+  @override
+  Stream<List<Contact>> constactList() => _constactList.stream;
+}
---
+++ packages/contact_list_repository/lib/src/repositories/contacts_repository_firestore.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import './repositories.dart';
+import '../models/models.dart';
+
+class ContactListRepositoryFireStore extends ContactListRepository {
+  final StreamController<List<Contact>> _loadedContactList =
+      StreamController<List<Contact>>();
+
+  final List<Contact> _cache = [];
+
+  @override
+  void dispose() {
+    _loadedContactList.close();
+  }
+
+  @override
+  void refresh() {
+    if (FirebaseFirestore.instance != null) {
+      FirebaseFirestore.instance
+          .collection('contactList')
+          .orderBy('lastname')
+          .snapshots()
+          .listen((snapshot) {
+        _cache.clear();
+        for (final document in snapshot.docs) {
+          _cache.add(
+            Contact(
+              name: document.data()['name'] as String,
+              lastname: document.data()['lastname'] as String,
+              mobile: document.data()['mobile'] as String,
+              mail: document.data()['mail'] as String,
+              address: document.data()['address'] as String,
+              description: document.data()['description'] as String,
+            ),
+          );
+        }
+
+        _loadedContactList.add(_cache);
+      });
+    }
+  }
+
+  @override
+  Stream<List<Contact>> constactList() => _loadedContactList.stream;
+}
