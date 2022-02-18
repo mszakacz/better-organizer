d5abb8733e90c13ca9024d7854cda8c357fcd104
Maciej <mszakacz@gmail.com>
2022-02-18 14:49:27 +0100

    fix: fixing props in NewContactEvent

--- lib/new_contact/bloc/new_contact_event.dart
+++ lib/new_contact/bloc/new_contact_event.dart
@@ -7,34 +7,57 @@ abstract class NewContactEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class AddContact extends NewContactEvent {}
+class AddContact extends NewContactEvent {
+  const AddContact();
+
+  @override
+  List<Object> get props => [];
+}
 
 class NameEditing extends NewContactEvent {
   final String name;
   const NameEditing(this.name);
+
+  @override
+  List<Object> get props => [name];
 }
 
 class LastnameEditing extends NewContactEvent {
   final String lastname;
   const LastnameEditing(this.lastname);
+
+  @override
+  List<Object> get props => [lastname];
 }
 
 class MobileEditing extends NewContactEvent {
   final String mobile;
   const MobileEditing(this.mobile);
+
+  @override
+  List<Object> get props => [mobile];
 }
 
 class MailEditing extends NewContactEvent {
   final String mail;
   const MailEditing(this.mail);
+
+  @override
+  List<Object> get props => [mail];
 }
 
 class AddressEditing extends NewContactEvent {
   final String address;
   const AddressEditing(this.address);
+
+  @override
+  List<Object> get props => [address];
 }
 
 class DesctriptionEditing extends NewContactEvent {
   final String description;
   const DesctriptionEditing(this.description);
+
+  @override
+  List<Object> get props => [description];
 }
