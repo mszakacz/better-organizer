32b6018b41e735e64f0ac90629ba8993a457e15a
Maciej <mszakacz@gmail.com>
2022-02-20 11:26:13 +0100

    feat(edit-contact): input editing event handler

--- lib/edit_contact/bloc/edit_contact_bloc.dart
+++ lib/edit_contact/bloc/edit_contact_bloc.dart
@@ -13,6 +13,12 @@ class EditContactBloc extends Bloc<EditContactEvent, EditContactState> {
           contact: Contact(),
         )) {
     on<GetContact>(_onGetContact);
+    on<NameEditing>(_onNameEditing);
+    on<LastnameEditing>(_onLastnameEditing);
+    on<MobileEditing>(_onMobileEditing);
+    on<MailEditing>(_onMailEditing);
+    on<AddressEditing>(_onAddressEditing);
+    on<DesctriptionEditing>(_onDescriptionEditing);
   }
 
   void _onGetContact(GetContact event, Emitter<EditContactState> emit) {
@@ -20,5 +26,34 @@ class EditContactBloc extends Bloc<EditContactEvent, EditContactState> {
       status: EditContactStatus.editing,
       contact: event.contact,
     ));
+  }
+
+  void _onNameEditing(NameEditing event, Emitter<EditContactState> emit) {
+    emit(state.copyWith(contact: state.contact.copyWith(name: event.name)));
+  }
+
+  void _onLastnameEditing(
+      LastnameEditing event, Emitter<EditContactState> emit) {
+    emit(state.copyWith(
+        contact: state.contact.copyWith(lastname: event.lastname)));
+  }
+
+  void _onMobileEditing(MobileEditing event, Emitter<EditContactState> emit) {
+    emit(state.copyWith(contact: state.contact.copyWith(mobile: event.mobile)));
+  }
+
+  void _onMailEditing(MailEditing event, Emitter<EditContactState> emit) {
+    emit(state.copyWith(contact: state.contact.copyWith(mail: event.mail)));
+  }
+
+  void _onAddressEditing(AddressEditing event, Emitter<EditContactState> emit) {
+    emit(state.copyWith(
+        contact: state.contact.copyWith(address: event.address)));
+  }
+
+  void _onDescriptionEditing(
+      DesctriptionEditing event, Emitter<EditContactState> emit) {
+    emit(state.copyWith(
+        contact: state.contact.copyWith(description: event.description)));
   }
 }
