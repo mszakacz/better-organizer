4a246d6b84c3aeb6726636737abfb0509a0aaad4
Maciej <mszakacz@gmail.com>
2022-02-22 22:15:08 +0100

    test(contact-list): ContactListEvent tests

---
+++ test/contact_list/bloc/contact_list_event_test.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:better_organizer/contact_list/contact_list.dart';
+import 'package:contact_list_repository/contact_list_repository.dart';
+
+void main() {
+  group('ContactListEvent', () {
+    const mockContactsList = [
+      Contact(
+        id: '1',
+        name: 'name1',
+        lastname: 'lastname1',
+        mobile: 'mobile1',
+        mail: 'mail1',
+        address: 'address1',
+        description: 'description1',
+      ),
+      Contact(
+        id: '2',
+        name: 'name2',
+        lastname: 'lastname2',
+        mobile: 'mobile2',
+        mail: 'mail2',
+        address: 'address2',
+        description: 'description2',
+      ),
+    ];
+
+    group('GetContactListEvent', () {
+      test('supports value equality', () {
+        expect(
+          GetContactListEvent(contactList: mockContactsList),
+          equals(
+            GetContactListEvent(contactList: mockContactsList),
+          ),
+        );
+      });
+
+      test('props are correct', () {
+        expect(
+          GetContactListEvent(contactList: mockContactsList).props,
+          equals(
+            <Object?>[mockContactsList], // contactList
+          ),
+        );
+      });
+    });
+
+    group('SearchEvent', () {
+      test('supports value equality', () {
+        expect(
+          SearchEvent(searchingWord: 'searchingWord'),
+          equals(
+            SearchEvent(searchingWord: 'searchingWord'),
+          ),
+        );
+      });
+
+      test('props are correct', () {
+        expect(
+          SearchEvent(searchingWord: 'searchingWord').props,
+          equals(
+            <Object?>['searchingWord'],
+          ),
+        );
+      });
+    });
+  });
+}
